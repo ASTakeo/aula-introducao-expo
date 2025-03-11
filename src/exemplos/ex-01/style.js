0b6ed8880acd06a9bd4688cde169182e78031bb4
ASTakeo <110730008+ASTakeo@users.noreply.github.com>
2025-03-11 20:33:34 -0300

    Aula - Exemplo: 1

--- src/exemplos/ex-01/style.js
+++ src/exemplos/ex-01/style.js
@@ -1,9 +1,31 @@
 import { StyleSheet } from 'react-native';
+import { RFPercentage } from 'react-native-responsive-fontsize';
 
 const styles = StyleSheet.create({
     container: {
         backgroundColor: '#fff',
+        padding: 8,
+        flex: 1,
+        alignItems: 'center',
+        // justifyContent: 'space-between',
     },
+    titulo: {
+        fontSize: RFPercentage(3),
+        fontWeight: 'bold',
+        borderWidth: 2,
+        borderColor: 'deepskyblue',
+        marginTop: 10,
+        marginBottom: 10,
+        padding: 4,
+        width: '100%',
+        height: 80,
+        textAlign: 'center',
+        textAlignVertical: 'center',
+    },
+    texto: {
+        fontSize: RFPercentage(2.2),
+        color: 'deepskyblue' 
+    }
 })
 
 export default styles;
