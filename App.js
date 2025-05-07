9d4c88c377797b4268ed06106a238e32d50c5e3a
ASTakeo <110730008+ASTakeo@users.noreply.github.com>
2025-05-07 20:35:36 -0300

    Aula - 07052025
    Atividade 6

--- App.js
+++ App.js
@@ -9,17 +9,19 @@ import Exemplo2 from './src/exemplos/ex-02';
 import Exemplo3 from './src/exemplos/ex-03';
 import Exemplo4 from './src/exemplos/ex-04';
 import Exemplo5 from './src/exemplos/ex-05';
+import Exemplo6 from './src/exemplos/ex-06';
 
 // Atividades
 import Atividade1 from './src/atividades/atividade-01';
 import Atividade2 from './src/atividades/atividade-02';
 import Atividade4 from './src/atividades/atividade-04';
+import Atividade6 from './src/atividades/atividade-06';
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <Text>Open up App.js to start working on your app!</Text> */}
-      <Exemplo5/>
+      <Atividade6/>
       <StatusBar style="light" />
     </View>
   );
---
+++ src/atividades/atividade-06/index.js
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import { View, Text } from 'react-native';
+
+import Input from './input';
+import Botao from './botao';
+
+import styles from './style';
+
+export default function Atividade6() {
+
+    const [peso, setPeso] = useState(null);
+    const [altura, setAltura] = useState(null);
+    const [imc, setImc] = useState(0.00);
+    const [valida, setValida] = useState('');
+    const mensErro = 'Valores inseridos de forma incorreta!';
+
+    function calculaImc() {
+        const tmpImc = peso / (altura * altura);
+        setImc(tmpImc);
+        resultadoimc(tmpImc);
+    }
+    
+    function resultadoimc(imc) {
+        switch (imc) {
+                case imc <= 18.5:
+                    return setResultado('Abaixo do Peso')
+                case imc >= 18.5 || imc <= 24.9:
+                    return setResultado('Abaixo do Peso')
+                // case '*':
+                //     return setResultado('Abaixo do Peso')
+                // case '/':
+                //     return setResultado('Abaixo do Peso')
+            
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.titulo}>Exemplo 6</Text>
+            <View style={styles.ladoalado}>
+                <Input placeholder='Peso' valor={peso} atualizaValor={setPeso} />
+                <Input placeholder='Altura' valor={altura} atualizaValor={setAltura} />
+            </View>
+
+            <Text style={styles.imc}>{isNaN(imc) ? mensErro : imc.toFixed(2)}</Text>
+
+            <Text style={[styles.imc, styles.txtMensagem]}>valida.toFixed(2)</Text>
+
+            <Botao calcular={calculaImc}>Calcular</Botao>
+        </View>
+    )
+}
